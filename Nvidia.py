7af32d0bab9e93e4e8321f9c982aafda4c8d2c0f
Olavgutten <ofurnes4@gmail.com>
2025-11-16 13:27:19 +0100

    Normalfordeling aksjer

---
+++ Nvidia.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 16 13:24:14 2025
+
+@author: ofurn
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 16 13:00:51 2025
+
+@author: ofurn
+"""
+
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+aapl = yf.download("NVDA", start="2025-01-01", end="2025-11-16")
+
+print(aapl)
+
+
+plt.plot(aapl.index, aapl["Close"])
+plt.plot(aapl.index, aapl["High"])
+plt.plot(aapl.index, aapl["Low"])
+plt.show()
+plt.close()
+
+returns = aapl["Close"].pct_change()
+
+plt.plot(returns.index, returns)
+plt.show()
+plt.close()
+
+
+plt.hist(returns, bins=40, edgecolor="black")
+plt.show()
+
+
+
